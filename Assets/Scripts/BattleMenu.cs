81da00aea3de56fdbe807c6d2c49821cc5ecd550
Andrew <andrew.m.boza@gmail.com>
2018-05-19 14:04:00 -0400

    Adds button prefab with blinking select cursor

---
+++ Assets/Scripts/BattleMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BattleMenu : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		EventSystem.current.SetSelectedGameObject(GetComponentsInChildren<Button>()[0].gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
