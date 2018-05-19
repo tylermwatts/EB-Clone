62c429d2b97af8983d9566c6f9504b4057e0c9bc
Andrew <andrew.m.boza@gmail.com>
2018-05-19 14:05:13 -0400

    Removes unnecessary code

--- Assets/Scripts/BattleMenu.cs
+++ Assets/Scripts/BattleMenu.cs
@@ -1,21 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class BattleMenu : MonoBehaviour 
 {
-
-	// Use this for initialization
 	void Start () 
 	{
 		EventSystem.current.SetSelectedGameObject(GetComponentsInChildren<Button>()[0].gameObject);
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		
 	}
 }
