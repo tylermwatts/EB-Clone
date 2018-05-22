ea93c33c7ff78b71c92b14ac9778bb2eaa92b21b
Tyler Watts <tylermwatts@gmail.com>
2018-05-22 18:34:59 -0500

    Created character stats script

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+	public List <string> inventoryList;
+
+	// 1 IQ point = 5 PP
+	// Guts = critical hit chance
+	// Luck = chance to hit and dodge
+	// Vitality = determines max HP
+	// Speed = Determines who goes first in battles, also raises dodge chance
+	// Offense = damage dealt
+	// Defense = damage reduction from PHYSICAL sources. Does not affect damage from PSI.
+	private int iq, guts, luck, vitality, speed, offense, defense;
+	private int characterLevel, nextLevel, exp, hp, pp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Item {
+
+	public string ItemName{get;set;}
+	public int RecoversHP{get;set;}
+	public bool IsReusable{get;set;}
+}
