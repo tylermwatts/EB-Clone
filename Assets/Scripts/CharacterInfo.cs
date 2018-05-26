13f9f89c5380835be77f8e57e103bb2420842b0e
Tyler Watts <tylermwatts@gmail.com>
2018-05-25 21:57:40 -0500

    Character experience WIP

--- Assets/Scripts/CharacterInfo.cs
+++ Assets/Scripts/CharacterInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class CharacterInfo : MonoBehaviour {
 
 	public List <Item> inventoryList = new List<Item>();
 	public CharacterName characterName;
+	public ExpChart expChart;
 
 	// 1 IQ point = 5 PP
 	// Guts = critical hit chance
@@ -14,8 +16,8 @@ public class CharacterInfo : MonoBehaviour {
 	// Speed = Determines who goes first in battles, also raises dodge chance
 	// Offense = damage dealt
 	// Defense = damage reduction from PHYSICAL sources. Does not affect damage from PSI.
-	private int iq, guts, luck, vitality, speed, offense, defense;
-	private int characterLevel, expToNextLevel, exp, hp, pp;
+	[SerializeField] private int iq, guts, luck, vitality, speed, offense, defense;
+	[SerializeField] private int characterLevel, currentEXP, maxHP, maxPP, currentHP, currentPP;
 
 	// Use this for initialization
 	void Start () {
@@ -32,15 +34,41 @@ public class CharacterInfo : MonoBehaviour {
 		inventoryList.Add(item3);
 	}
 
-	public void PutItemInInventory(Item item){
+	void Update (){
+		// if (currentEXP >= expChart.expToLevel[characterLevel - 1]){
+		//		LevelUp();
+		// }
+	}
+
+    public void PutItemInInventory(Item item){
 		inventoryList.Add(item);
 	}
 
-	public void LevelUp(){
-		if (exp >= expToNextLevel){
-			characterLevel++;
-			expToNextLevel += (exp * 2);
+	private void LevelUp(){
+		characterLevel++;
+
+		#region Base Stats Increase
+		iq += UnityEngine.Random.Range(1,5);
+		guts += UnityEngine.Random.Range(1,5);
+		luck += UnityEngine.Random.Range(1,5);
+		vitality += UnityEngine.Random.Range(1,5);
+		speed += UnityEngine.Random.Range(1,5);
+		offense += UnityEngine.Random.Range(1,5);
+		defense += UnityEngine.Random.Range(1,5);
+		#endregion
+		
+		if (vitality % 5 == 0){
+			maxHP++;
 		}
+
+		if (iq % 5 == 0){
+			maxPP++;
+		}
+
+	}
+
+	public void AwardExperiencePoints(int exp){
+		currentEXP += exp;
 	}
 
 }
