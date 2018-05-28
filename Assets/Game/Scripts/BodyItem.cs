db5f58769c1c5b3e03f2c3288fb73e5dcad34417
Tyler Watts <tylermwatts@gmail.com>
2018-05-28 12:52:35 -0500

    Creating Item Classes WIP

---
+++ Assets/Game/Scripts/BodyItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyItem : Item {
+
+	public int IncreaseDefenseBy {get;set;}
+
+	public int IncreaseSpeedBy {get;set;}
+
+	// TODO: add "Protection From" property once Status Ailments are created
+}
