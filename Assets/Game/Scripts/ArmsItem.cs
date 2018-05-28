202dad16f231d67da91697687b4e9cf7c342d1e7
Tyler Watts <tylermwatts@gmail.com>
2018-05-28 12:52:35 -0500

    Creating Item Classes WIP

---
+++ Assets/Game/Scripts/ArmsItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmsItem : Item {
+
+	public int IncreaseDefenseBy {get;set;}
+
+	public int IncreaseLuckBy {get;set;}
+
+	/* 	Some Arms items also offer protection from Hypnosis. We may want to add
+		an enum of possible status ailments, and add a "ProtectionFrom" property
+		to items that protect from certain ailments.
+	*/
+}
---
+++ Assets/Game/Scripts/HealingItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingItem : Item {
+
+	public int RecoversHP {get;set;}
+
+	public int RecoversPP {get;set;}
+	
+	public int NumberOfUses {get;set;}
+}
