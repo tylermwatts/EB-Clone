20a4eb24f0d6971a24a7ff461f71592cf13b5e77
Tyler Watts <tylermwatts@gmail.com>
2018-06-02 10:56:41 -0500

    Error catching for Doorway script in case no scene specified

--- Assets/Game/Scripts/Doorway.cs
+++ Assets/Game/Scripts/Doorway.cs
@@ -8,6 +8,15 @@ public class Doorway : MonoBehaviour {
 	public string scene;
 
 	public void NextScene () {
-		SceneManager.LoadScene(scene);
+		if (scene == null || scene == "")
+		{
+			Debug.LogWarning ("No scene specified for doorway transition!");
+		}
+		else
+		{
+			SceneManager.LoadScene(scene);
+		}
+
 	}
+
 }
